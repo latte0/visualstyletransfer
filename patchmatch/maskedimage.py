b7781a3e6b43b649355744398a0bf7bdaeece9fc
jinbo <jinbo_kazuyuki@live2d.com>
2017-05-27 19:01:39 +0900

    revise

--- patchmatch/maskedimage.py
+++ patchmatch/maskedimage.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 class MaskedImage:
     def __init__(self):
-      self.mask = [[]]
+      self.mask = [[[]]]
       self.image = Image.load()
       self.similarity = []
       self.isNew = 0
@@ -27,7 +27,7 @@ class MaskedImage:
 
         G_initSim = 1
 
-    def initSimilarity2(self,):
+    def initSimilarity2(self):
         i, length = 0, 0
 
 
@@ -45,5 +45,87 @@ class MaskedImage:
 
         return mIm
 
-    def getSampleMaskedImage(x, y, band):
-        return data[x*step+y]
+    def isMasked(self, x, y):
+        if mIm == None or mIm.mask == None:
+            return 0
+        return mim.mask[x][y]
+
+    def setMask(self, x, y, value):
+        if mIm == None or mIm.mask == None:
+            return
+## what does it mean?
+##        mIm.mask[x][y]=0 < value
+
+    def distance(self, xs, ys, target, xt, yt, S):
+        distance = 0
+        wsum = 0
+        ssdmax  =9*255*255
+        dy, dx, band = 0, 0, 0
+        xks, yks = 0, 0
+        xkt, ykt = 0, 0
+        ssd = 0.0
+        res = 0.0
+        s_value, t_value, s_gx, t_gx, s_gy, t_gy = 0, 0, 0, 0, 0, 0
+
+        for dy in range(-S, S):
+            for dx in range(-S, S):
+                xks = xs+dx
+    			yks = ys+dy
+    			xkt=xt+dx
+    			ykt=yt+dy
+    			wsum += 1
+
+    			if ( xks<1 || xks>=source.image.height-1 ):
+                    distance += 1
+                    continue
+    			if ( yks<1 || yks>=source.image.width-1 ):
+                    distance += 1
+                    continue
+
+                if isMasked(xks, yks):
+                    distance += 1
+                    continue
+
+    			if (xkt<1 or xkt>=target.image.height-1):
+                    distance++
+                    continue
+    			if (ykt<1 or ykt>=target.image.width-1):
+                    distance++
+                    continue
+
+			    if target.isMasked(xkt, ykt):
+                    distance++
+                    continue
+
+
+    			ssd=0
+                for (band=0 band<3 ++band):
+
+    				s_value = getSampleMaskedImage(source, xks, yks, band)
+    				t_value = getSampleMaskedImage(source, xkt, ykt, band)
+
+    				s_gx = 128+(getSampleMaskedImage(source, xks+1, yks, band) - getSampleMaskedImage(source, xks-1, yks, band))/2
+    				t_gx = 128+(getSampleMaskedImage(target, xkt+1, ykt, band) - getSampleMaskedImage(target, xkt-1, ykt, band))/2
+
+    				s_gy = 128+(getSampleMaskedImage(source, xks, yks+1, band) - getSampleMaskedImage(source, xks, yks-1, band))/2
+    				t_gy = 128+(getSampleMaskedImage(target, xkt, ykt+1, band) - getSampleMaskedImage(target, xkt, ykt-1, band))/2
+
+    				ssd = ssd + pow(float(s_value-t_value) , 2) // distance between values in [0,255^2]
+    				ssd = ssd + pow(float(s_gx-t_gx) , 2) // distance between Gx in [0,255^2]
+    				ssd = ssd + pow(float(s_gy-t_gy) , 2) // distance between Gy in [0,255^2]
+
+                distance += ssd/ssdmax
+
+    	res = int(DSCALE*distance/wsum)
+    	if (res < 0 or res > DSCALE) return DSCALE
+    	return res
+
+
+    def getSampleMaskedImage(self, x, y, band):
+        return data[x][y]
+
+    def upscale(self, newW, nweH):
+        x, y = 0, 0
+        xs, ys = 0, 0
+        H, W = 0, 0
+        H= sdop
